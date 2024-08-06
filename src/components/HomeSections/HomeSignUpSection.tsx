6c6e2bcbaffa2c4ec34b4513c629903719aca492
lukblan <luk.blanko@gmail.com>
2024-08-06 10:29:31 -0300

    Fix blur home

--- src/components/HomeSections/HomeSignUpSection.tsx
+++ src/components/HomeSections/HomeSignUpSection.tsx
@@ -3,7 +3,7 @@ import {GuessLogin} from "@/components/Login/GuessLogin";
 function HomeSignUpSection() {
   return (
     <section className="grid lg:grid-cols-2 pt-24 justify-center bg-wave p-4 min-h-screen gap-8">
-      <div className="flex flex-col gap-8 text-white text-center">
+      <div className="flex flex-col gap-8 text-white text-center justify-items-center">
         <div>
           <h2 className="font-bold text-3xl">Trello brings all your tasks, teammates, and tools together</h2>
           <p>Keep everything in the same place—even if your team isn’t.</p>
@@ -15,7 +15,7 @@ function HomeSignUpSection() {
         </div>
       </div>
 
-      <img src="TrelloUICollage_4x.png" />
+      <img className="w-0 min-w-full" src="TrelloUICollage_4x.webp" />
     </section>
   )
 }
