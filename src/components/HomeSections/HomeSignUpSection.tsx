28c2a0ab7c1fb16c0ca6f44f6ce5ba7e6dcb6884
lukblan <luk.blanko@gmail.com>
2024-08-06 11:47:17 -0300

    Adjust home sign up section styles on mobile and tablet

--- src/components/HomeSections/HomeSignUpSection.tsx
+++ src/components/HomeSections/HomeSignUpSection.tsx
@@ -2,15 +2,15 @@ import {GuessLogin} from "@/components/Login/GuessLogin";
 
 function HomeSignUpSection() {
   return (
-    <section className="grid lg:grid-cols-2 pt-24 justify-center bg-wave p-4 min-h-screen gap-8">
-      <div className="flex flex-col gap-8 text-white text-center justify-items-center">
-        <div>
-          <h2 className="font-bold text-3xl">Trello brings all your tasks, teammates, and tools together</h2>
-          <p>Keep everything in the same place—even if your team isn’t.</p>
+    <section className="grid lg:grid-cols-2 pt-16 justify-center bg-wave p-4 gap-8">
+      <div className="flex flex-col gap-8 text-white justify-items-center items-center">
+        <div className="grid gap-4 text-center lg:text-left max-w-[30rem] md:max-w-[40rem]">
+          <h2 className="font-semibold text-3xl md:text-4xl lg:text-6xl">Trello brings all your tasks, teammates, and tools together</h2>
+          <p className="text-xl">Keep everything in the same place-even if your team isn’t.</p>
         </div>
 
-        <div className="flex max-w-[80rem]">
-          <input className="hidden md:block" type="email" />
+        <div className="flex w-full md:w-3/4 gap-4">
+          <input className="hidden md:block basis-2/3 pl-4" type="email" placeholder="email" />
           <GuessLogin className={"rounded-md p-3"} />
         </div>
       </div>
